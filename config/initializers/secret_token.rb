a7b8b79a0d40c91819573db1fc30858e9a88c9f6
Nam Chu Hoai <nambrot@googlemail.com>
2011-10-30 19:30:16 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fbphotos::Application.config.secret_token = '9affb632d22545c4acd78a2857f90c538802584d3341065d8a19402457e6a6e8c97066f6502f3549dd55f3bc47a1c6fe965026723c740329ac1e3ef0d65d4706'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Fbphotos::Application.config.session_store :cookie_store, key: '_fbphotos_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Fbphotos::Application.config.session_store :active_record_store
